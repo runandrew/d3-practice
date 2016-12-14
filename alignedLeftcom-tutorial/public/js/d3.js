fc771e23058fef04b5bc7cac9af39b0d6f10d756
Andrew Garcia <runandrew@gmail.com>
2016-12-13 19:43:24 -0500

    printing out data objects

--- alignedLeftcom-tutorial/public/js/d3.js
+++ alignedLeftcom-tutorial/public/js/d3.js
@@ -1,1 +1,11 @@
-d3.select("body").append("p").text("New paragraph! YEP, it WORKS");
+/* global d3 */
+
+const dataset = [5, 10, 15, 20, 25];
+
+d3.select('body')
+.selectAll('p')
+.data(dataset)
+.enter()
+.append('p')
+.text(data => data)
+.style('color', data => data > 15 ? 'red' : 'black');
