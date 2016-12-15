7031969e4ddf64dfa1111e7da809063d2b562786
Andrew Garcia <runandrew@gmail.com>
2016-12-14 20:11:23 -0500

    completed up to updating scales

--- alignedLeftcom-tutorial/public/js/d3-barchart.js
+++ alignedLeftcom-tutorial/public/js/d3-barchart.js
@@ -3,17 +3,10 @@
 // Constants
 
 // SVG setup
-const width = 500;
-const height = 100;
+const width = 1200;
+const height = 250;
 
-const barWidth = 20;
-const barPadding = 1;
-
-// Dataset creation
-const dataset = [];
-for (let i = 0; i < 30; i++) {
-    dataset[i] = Math.floor(Math.random() * 51);
-}
+const maxNumber = 50;
 
 // SVG Construction - Bar chart
 const svg = d3.select('body')
@@ -21,14 +14,37 @@ const svg = d3.select('body')
 .attr('width', width)
 .attr('height', height);
 
+// Dataset creation
+const dataset = [];
+createNewDataValues();
+
+function createNewDataValues() {
+    for (let i = 0; i < 60; i++) {
+        dataset[i] = Math.floor(Math.random() * (maxNumber + 1));
+    }
+    updateDataValues();
+}
+
+// Axes
+const xScale = d3.scaleBand()
+.domain(d3.range(dataset.length))
+.rangeRound([0, width])
+.paddingInner(0.05);
+
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(dataset, data => data)])
+.range([height, 0])
+.nice();
+
+
 svg.selectAll('rect')
 .data(dataset)
 .enter()
 .append('rect')
-.attr('x', (data, i) => i * (width / dataset.length))
-.attr('y', (data) => height - data * 2)
-.attr('width', width / dataset.length - barPadding)
-.attr('height', (data) => data * 2)
+.attr('x', (data, i) => xScale(i))
+.attr('y', (data) => yScale(data))
+.attr('width', xScale.bandwidth())
+.attr('height', (data) => height - yScale(data))
 .attr('fill', (data) => `rgb(0, 0, ${ data * 10 })`);
 
 svg.selectAll('text')
@@ -36,9 +52,32 @@ svg.selectAll('text')
 .enter()
 .append('text')
 .text(data => data)
-.attr('x', (data, i) => i * (width / dataset.length) + (width / dataset.length - barPadding) / 2)
-.attr('y', (data) => height - (data * 2) + 14)
+.attr('x', (data, i) => xScale(i) + xScale.bandwidth() / 2)
+.attr('y', (data) => yScale(data) + 14)
 .attr('font-family', 'sans-serif')
 .attr('font-size', '11px')
 .attr('fill', 'white')
 .attr('text-anchor', 'middle');
+
+// Event listener
+d3.select('p')
+.on('click', createNewDataValues);
+
+function updateDataValues() {
+    svg.selectAll('rect')
+    .data(dataset)
+    .transition()
+    // .delay((data, i) => i * 50)
+    .duration(500)
+    .attr('y', data => yScale(data))
+    .attr('height', data => height - yScale(data))
+    .attr('fill', (data) => `rgb(0, 0, ${ data * 10 })`);
+
+    svg.selectAll('text')
+    .data(dataset)
+    .transition()
+    // .delay((data, i) => i * 50)
+    .duration(500)
+    .text(data => data)
+    .attr('y', data => yScale(data) + 14);
+}
--- alignedLeftcom-tutorial/public/js/d3-scatterplot.js
+++ alignedLeftcom-tutorial/public/js/d3-scatterplot.js
@@ -1,13 +1,43 @@
 /* global d3 */
 
-const dataset = [
-                [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-              ];
+const dataset = [];
+const numDataPoints = 50;
+const xRange = Math.random() * 1000;
+const yRange = Math.random() * 1000;
+for (let i = 0; i < numDataPoints; i++) {
+    let newNumber1 = Math.round(Math.random() * xRange);
+    let newNumber2 = Math.round(Math.random() * yRange);
+    dataset.push([newNumber1, newNumber2]);
+}
 
 // SVG parameters
-const svgWidth = 500;
-const svgHeight = 100;
+const svgWidth = 700;
+const svgHeight = 400;
+const padding = 30;
+
+// Scaling
+const xScale = d3.scaleLinear()
+.domain([0, d3.max(dataset, data => data[0])])
+.range([0 + padding, svgWidth - padding * 2])
+.nice();
+
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(dataset, data => data[1])])
+.range([svgHeight - padding, 0 + padding])
+.nice();
+
+const rScale = d3.scaleLinear()
+.domain([0, d3.max(dataset, data => data[1])])
+.range([2, 5])
+.nice();
+
+const xAxis = d3.axisBottom()
+.scale(xScale)
+.ticks(5);
+
+const yAxis = d3.axisLeft()
+.scale(yScale)
+.ticks(5);
 
 // SVG creation
 const svg = d3.select('body')
@@ -19,17 +49,28 @@ svg.selectAll('circle')
 .data(dataset)
 .enter()
 .append('circle')
-.attr('cx', data => data[0])
-.attr('cy', data => svgHeight - data[1])
-.attr('r', (data) => Math.sqrt(data[1]));
+.attr('cx', data => xScale(data[0]))
+.attr('cy', data => yScale(data[1]))
+.attr('r', (data) => rScale(data[1]));
 
-svg.selectAll('text')
-.data(dataset)
-.enter()
-.append('text')
-.text((data) => `${data[0]}, ${data[1]}`)
-.attr('x', data => data[0])
-.attr('y', data => svgHeight - data[1])
-.attr("font-family", "sans-serif")
-.attr("font-size", "11px")
-.attr("fill", "red");
+// Data point labels
+// svg.selectAll('text')
+// .data(dataset)
+// .enter()
+// .append('text')
+// .text((data) => `${data[0]}, ${data[1]}`)
+// .attr('x', data => xScale(data[0]))
+// .attr('y', data => yScale(data[1]))
+// .attr('font-family', 'sans-serif')
+// .attr('font-size', '11px')
+// .attr('fill', 'red');
+
+svg.append('g')
+.attr('class', 'axis')
+.attr('transform', `translate(0, ${svgHeight - padding})`)
+.call(xAxis);
+
+svg.append('g')
+.attr('class', 'axis')
+.attr('transform', `translate(${padding}, 0)`)
+.call(yAxis);
